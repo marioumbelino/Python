83bad1c27550761d33792ec6c3359e936532d907
Mário Umbelino <marioherbert2016@gmail.com>
2023-05-09 20:52:13 -0300

    Menor
    Adicionei o menor e utilizei o operador "and"

--- Desafios/Desafio 033.py
+++ Desafios/Desafio 033.py
@@ -1,13 +1,17 @@
 n1 = float(input('Digite um número: '))
 n2 = float(input('Digite outro número: '))
 n3 = float(input('Digite o terceiro número: '))
-if n1 > n2:
-    if n1 > n3:
-        print('O número {} é maior do que {} e {}'.format(n1, n2, n3))
-    else:
-        print('O número {} é maior do que {} e {}'.format(n3, n1, n2))
-else:
-    if n2 > n3:
-        print('O número {} é maior do que {} e {}'.format(n2, n1, n3))
-    else:
-        print('O número {} é maior do que {} e {}'.format(n3, n1, n2))
+# Qual é o maior
+if n1 > n2 and n1 > n3:
+    print('O número {} é o maior!'.format(n1))
+if n2 > n1 and n2 > n3:
+    print('O número {} é o maior!'.format(n2))
+if n3 > n1 and n3 > n2:
+    print('O número {} é o maior!'.format(n3))
+# Qual é o menor
+if n1 < n2 and n1 < n3:
+    print('O número {} é o menor!'.format(n1))
+if n2 < n1 and n2 < n3:
+    print('O número {} é o menor!'.format(n2))
+if n3 < n1 and n3 < n2:
+    print('O número {} é o menor!'.format(n3))
--- Desafios/Desafio 035.py
+++ Desafios/Desafio 035.py
@@ -1,14 +1,13 @@
+from time import sleep
+print('-=-' * 10)
+print('Analisador de Triângulos')
+print('-=-' * 10)
 r1 = float(input('Informe o comprimento de uma das retas: '))
 r2 = float(input('Informe o comprimento da segunda reta: '))
 r3 = float(input('Informe o comprimento da terceira reta: '))
-if r1 < r2 + r3:
-    if r2 < r1 + r3:
-        if r3 < r1 + r2:
-            print('Essas retas conseguem formar um triângulo!')
-        else:
-            print('Essas retas não conseguem formar um triângulo!')
-    else:
-        print('Essas retas não conseguem formar um triângulo!')
+print('Analisando, aguarde...')
+sleep(3)
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+     print('Essas retas CONSEGUEM formar um triângulo!')
 else:
-    print('Essas retas não conseguem formar um triângulo!')
-    print('Parabéns')
+    print('Essas retas NÃO conseguem formar um triângulo!')
