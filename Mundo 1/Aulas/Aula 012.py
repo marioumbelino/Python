8a5bdea32fc8f0404380f093e42c3191c7183be5
Mário Umbelino <marioherbert2016@gmail.com>
2023-05-11 20:53:57 -0300

    Aula 012

---
+++ Mundo 1/Aulas/Aula 012.py
@@ -0,0 +1,6 @@
+nome = str(input('Digite o seu nome: '))
+if nome == 'Mário':
+    print('Que nome lindo, Mário!')
+elif nome == 'Herbert':
+    print('Que nome lindo, Herbert!')
+print('Tenha um bom dia!!')
