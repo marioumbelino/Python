87eb86cb9d5fd15c6b92f27b541ca2ed9d4409a2
Mário Umbelino <marioherbert2016@gmail.com>
2023-05-18 18:15:05 -0300

    Leitor de Números Primos
    Programa que lê um número e informa se ele é primo ou não.

---
+++ Mundo 1/Desafios/desafio 052.py
@@ -0,0 +1,14 @@
+num = int(input('Digite um núemro: '))
+primo = 0
+for c in range (1, num+1):
+    if num % c == 0:
+        print('\033[36m', end='')
+        primo = primo + 1
+    else:
+        print('\033[m', end='')
+    print('{} '.format(c), end='')
+print('\n\033[mO número {} foi divido {} vezes'.format(num, primo))
+if primo == 2:
+    print('Logo, é um número primo.')
+else:
+    print('Logo, não é um número primo.')
