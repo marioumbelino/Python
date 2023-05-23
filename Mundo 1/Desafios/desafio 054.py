255a72a1b5f6b24f9712e431516e4f8d4dba356a
Mário Umbelino <marioherbert2016@gmail.com>
2023-05-22 21:43:52 -0300

    Contador de Maioridade
    Programa recebe sete anos de nascimento e informa quantos são maiores de idade e quantos são menores.

---
+++ Mundo 1/Desafios/desafio 054.py
@@ -0,0 +1,9 @@
+from datetime import date
+con = 0
+for c in range (1, 8):
+    nasc = int(input('Qual é o ano do seu nascimento?? '))
+    nasc = date.today().year - nasc
+    if nasc > 18:
+        con = con + 1
+print('{} pessoas são maiores de idade, e'.format(con))
+print('{} pessoas são menores de idade.'.format(7 - con))
