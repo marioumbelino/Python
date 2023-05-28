1a34ff9edd4500d531c196036ff5b87388fb84a6
Mário Umbelino <marioherbert2016@gmail.com>
2023-05-28 16:56:32 -0300

    Leitor de Sexto (M / F)
    Programa que lê o sexo de uma pessoa e, caso ela digite algo diferente de M ou F, o programa irá insistir na correção do erro.

---
+++ Mundo 1/Desafios/desafio 057.py
@@ -0,0 +1,10 @@
+sexo = ''
+while sexo != 'M' and sexo != 'F':
+    sexo = str(input('Digite o seu sexo [M/F]: ')).upper()
+    if sexo != 'M' and sexo != 'F':
+        print('Resposta incorreta. Por favor, tente novamente.')
+if sexo == 'F':
+    print('Seu sexo é feminino')
+else:
+    print('Seu sexo é masculino')
+print('Fim!')
