082485c8628a7200cc5945e9012671846933864e
Mário Umbelino <marioherbert2016@gmail.com>
2023-06-02 20:42:12 -0300

    Maior e Menor
    Programa lê um número inteiro e pergunta ao usuário se ele quer continuar. Ademais, o programa mostrará qual foi a média dos números digitados e qual foi o maior e o menor número digitado.

---
+++ Mundo 1/Desafios/desafio 065.py
@@ -0,0 +1,21 @@
+res = 's'
+num = 0
+soma = 0
+count = 0
+maior = 0
+menor = 0
+while res in 'Ss':
+    num = int(input('Digite um número inteiro: '))
+    res = str(input('Você deseja continuar [S/N]? '))
+    soma += num
+    count += 1
+    if count == 1:
+        maior = num
+        menor = num
+    elif maior < num:
+        maior = num
+    elif menor > num:
+        menor = num
+media = soma / count
+print('A soma dos números digitados resulta em {} e possui uma média de {}.'.format(soma, media))
+print('O maior número digitado foi {}, enquanto que o menor foi {}.'.format(maior, menor))
