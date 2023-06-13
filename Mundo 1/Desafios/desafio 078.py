818226ba294bdc93695d06f794448ad12449f309
Mário Umbelino <marioherbert2016@gmail.com>
2023-06-13 14:20:02 -0300

    > e < em Listas
    Programa que lê 5 números e mostra os valores digitados e ainda qual é o menor e o maior valor digitado e suas posições.

---
+++ Mundo 1/Desafios/desafio 078.py
@@ -0,0 +1,14 @@
+valores = []
+
+for r in range(0, 5):
+    valores.append(int(input('Digite um valor: ')))
+maior = max(valores)
+print(f'Você digitou os valores {valores}')
+print(f'O maior valor digitado foi {max(valores)}, na(s) posição(ões)', end=' ')
+for p, v in enumerate(valores):
+    if v == max(valores):
+        print(f'{p + 1}...', end=' ')
+print(f'\nO menor valor digitado foi {min(valores)}, na(s) posição(ões):', end=' ')
+for p, v in enumerate(valores):
+    if v == min(valores):
+        print(f'{p + 1}...', end=' ')
