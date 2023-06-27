e34e1a89154336e71135568f579ff79281856449
Mário Umbelino <marioherbert2016@gmail.com>
2023-06-27 19:59:11 -0300

    Recuperação
    Adicionei a situação "Recuperação" para os alunos que possuem uma média entre 5 e 7.

--- Mundo 1/Desafios/desafio 090.py
+++ Mundo 1/Desafios/desafio 090.py
@@ -1,10 +1,12 @@
 nome = str(input('Digite o nome do aluno: '))
-média = float(input('Digite a média do aluno: '))
+média = float(input(f'Digite a média do(a) {nome}: '))
 aluno = {'Nome': nome, 'Média': média}
 if média >= 7:
     aluno['Situação'] = '\033[32mAprovado\033[m'
+elif 5 <= média < 7:
+    aluno['Situação'] = '\033[36mRecuperação\033[m'
 else:
     aluno['Situação'] = '\033[31mReprovado\033[m'
 print(f'O nome do aluno é {nome}')
 print(f'A sua média é {média}')
-print(f'Ele(a) foi {aluno["Situação"]}')
+print(f'Sua situação é {aluno["Situação"]}')
