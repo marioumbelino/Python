57e8a6036d16c354c7e447a6d34d930982115957
Mário Umbelino <marioherbert2016@gmail.com>
2023-06-28 19:35:15 -0300

    Teste

--- Mundo 1/Desafios/desafio 092.py
+++ Mundo 1/Desafios/desafio 092.py
@@ -1,9 +1,9 @@
 import datetime
-nome = str(input('Digite o nome: '))
+pessoas = []
+pessoas['Nome'] = str(input('Digite o nome: '))
 nasc = int(input('Digite o ano de nascimento: '))
 ctps = str(input('Você tem carteira de trabalho? [S/N] ')).strip()[0]
-idade = datetime.date.today().year - nasc
-pessoa = {'Nome': nome, 'Idade': idade, 'Carteira de Trabalho': ctps}
+Pessoas['Idade'] = datetime.date.today().year - nasc
 if ctps in 'Ss':
     numctps = int(input('Digite o número da sua carteira de trabalho: '))
     contract = int(input('Digite o ano de contratação: '))
@@ -12,11 +12,11 @@ if ctps in 'Ss':
     pessoa['Salário'] = sal
     apos = contract - nasc + 35
 print('-' * 20, 'Resultado', '-' * 20)
-print(f'O nome cadastrado é {nome}.')
-print(f'Sua idade é {idade} anos.')
+print(f'O nome cadastrado é {pessoas["Nome"]}.')
+print(f'Sua idade é {pessoas['Idade]} anos.')
 if ctps in 'Ss':
     print(f'O número da sua carteira de trabalho é: {numctps}')
-    print(f'Foi contratado no ano de {contract} e possui um salário de R${sal}.')
+    print(f'Foi contratado no ano de {contract} e possui um salário de R${sal:.2f}.')
     print(f'Com isso, poderá se aposentar com {apos} anos.')
 else:
     print('A pessoa cadastrada não possui carteira de trabalho.')
