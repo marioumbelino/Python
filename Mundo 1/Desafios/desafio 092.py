07133a671fc2eb3b6341e6f175f2c1c70d86f1a8
Mário Umbelino <marioherbert2016@gmail.com>
2023-06-28 19:36:36 -0300

    Cadastro do Trabalhador
    Programa que lê algumas informações de uma pessoa e ao final mostrará dados referente a sua vida profissional, como, por exemplo, o ano no qual irá se aposentar.

--- Mundo 1/Desafios/desafio 092.py
+++ Mundo 1/Desafios/desafio 092.py
@@ -1,9 +1,9 @@
 import datetime
-pessoas = []
-pessoas['Nome'] = str(input('Digite o nome: '))
+nome = str(input('Digite o nome: '))
 nasc = int(input('Digite o ano de nascimento: '))
 ctps = str(input('Você tem carteira de trabalho? [S/N] ')).strip()[0]
-Pessoas['Idade'] = datetime.date.today().year - nasc
+idade = datetime.date.today().year - nasc
+pessoa = {'Nome': nome, 'Idade': idade, 'Carteira de Trabalho': ctps}
 if ctps in 'Ss':
     numctps = int(input('Digite o número da sua carteira de trabalho: '))
     contract = int(input('Digite o ano de contratação: '))
@@ -12,8 +12,8 @@ if ctps in 'Ss':
     pessoa['Salário'] = sal
     apos = contract - nasc + 35
 print('-' * 20, 'Resultado', '-' * 20)
-print(f'O nome cadastrado é {pessoas["Nome"]}.')
-print(f'Sua idade é {pessoas['Idade]} anos.')
+print(f'O nome cadastrado é {nome}.')
+print(f'Sua idade é {idade} anos.')
 if ctps in 'Ss':
     print(f'O número da sua carteira de trabalho é: {numctps}')
     print(f'Foi contratado no ano de {contract} e possui um salário de R${sal:.2f}.')
