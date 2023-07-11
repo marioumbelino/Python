e2f71cbe971bf8b645a41efc337b0c3ea5ab62cf
Mário Umbelino <marioherbert2016@gmail.com>
2023-07-10 21:15:15 -0300

    Contador
    Programa mostra a contagem entre um intervalo de números.

---
+++ Mundo 1/Desafios/desafio 098.py
@@ -0,0 +1,21 @@
+def contador(a, b, c):
+    lin()
+    c = abs(c)
+    if c == 0:
+        c += 1
+    print(f'Contagem de {a} a {b} de {abs(c)} em {abs(c)}:')
+    if b < a:
+        c = -c
+        b -= 2
+    for num in range(a, b + 1, c):
+        print(num, end=' ')
+    print('FIM!')
+    lin()
+def lin():
+    print('-' * 30)
+
+
+contador(1, 10, 1)
+contador(10, 0, 2)
+print('Agora é sua vez!!')
+contador(int(input('Início: ')), int(input('Final: ')), int(input('Passo: ')))
