c18ea79c55ad036f5a34eedfb864948e030e007f
Mário Umbelino <marioherbert2016@gmail.com>
2023-07-12 20:33:05 -0300

    Aula 021
    Rascunho da aula 021 do curso de python, do Curso em Vídeo.

---
+++ Mundo 1/Aulas/aula 021.py
@@ -0,0 +1,10 @@
+from math import factorial
+def fatorial(a):
+    fac = factorial(a)
+    return fac
+    
+r1 = factorial(5)
+r2 = factorial(10)
+r3 = factorial(3)
+
+print(f'Os fatoriais correspondentes são: {r1}, {r2}, {r3}')
